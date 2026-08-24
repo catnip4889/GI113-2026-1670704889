e9728db135882f3ef2d8db1eeb012ee91db0586a
catnip4889 <bking.ywing@gmail.com>
2026-08-24 16:28:03 +0700

    AddLab02Header

---
+++ Lab02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Student ID :  1670704889
+ * Name       :  Thunyaphon Amphaphan
+ * Section    :  129B
+ * No.        :  N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
